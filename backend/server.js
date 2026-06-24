a0eb8e80fde0e0d286acb5a2abbb78bf465af8c1
Srikeerthi <srikeerthidindu@gmail.com>
2026-06-24 14:04:02 +0530

    fix production login and api baseURL issues

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const app = express()
 
 app.use(cors({
     origin: [
+        "http://localhost:3000",
         "http://localhost:5173",
         "https://smart-campus-canteen-three.vercel.app"
     ],
